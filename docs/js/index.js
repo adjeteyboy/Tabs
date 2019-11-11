de771060438cdfa2b1544c360b25378c4c6063c9
adjeteyboy <adjeieric4@yahoo.com>
2019-11-11 17:20:56 +0000

    first commit mobile complete.

---
+++ docs/js/index.js
@@ -0,0 +1,36 @@
+// Get the modal
+var modal = document.getElementById("mobile-filter__modal");
+// Get the button that opens the modal
+var openFilterBtn = document.getElementById("mobile-filter__btn");
+// Get the btn that closes the modal
+var closeBtn = document.getElementById("mobile-filter__modal--submit-btn");
+// When the user clicks on the button, open the modal
+openFilterBtn.onclick = function() {
+  modal.style.display = "flex";
+}
+// When the user clicks on the closeBtn, close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    // console.log('yes')
+  }
+}
+
+
+function openNav() {
+  document.getElementById("sideNav").style.width = "300px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+
+
+$('#brands').sortable({
+  containment: 'parent',
+  cursor: 'grabbing'
+});
